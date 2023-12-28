9f897684a0c37188bf386afadaf14771b72e6ac7
Francisco Italo <italo.silvan87@gmail.com>
2023-12-28 20:13:11 -0300

    CPT-9: adicionar primeira versão da funcao getServeSide

--- capputeeno/src/pages/product/[id].tsx
+++ capputeeno/src/pages/product/[id].tsx
@@ -1,7 +1,38 @@
 import { useRouter } from 'next/router';
 import React from 'react';
+import { grapQLClient } from '../../services/graphiqlClient';
+import { gql } from 'graphql-request';
+import { Product } from '../../@types/types';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export default function Page() {
-  const router = useRouter();
-  return <p>Post: {router.query.id}</p>;
+export const getServerSideProps = (async (context: {
+  params: { id: string };
+}) => {
+  const { id } = context.params;
+
+  const response = await grapQLClient.request(gql`
+    query{
+      Product(id:"${id}") {
+        id
+        name
+        price_in_cents
+        description
+      }
+    } 
+    
+    `);
+
+  const product: Product = await response.json();
+
+  return { props: { product } };
+}) satisfies GetServerSideProps<{ product: Product }>;
+
+export default function Page({
+  product,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <p>Post</p>;<p>{product.name}</p>
+    </>
+  );
 }
