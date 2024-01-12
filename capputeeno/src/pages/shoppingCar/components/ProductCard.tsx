8812c9e4900935fcbff354776a0109f1fc5853b0
Francisco Italo <italo.silvan87@gmail.com>
2024-01-12 19:41:06 -0300

    CPT-21: adicionar ajustes

--- capputeeno/src/pages/shoppingCar/components/ProductCard.tsx
+++ capputeeno/src/pages/shoppingCar/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Box, IconButton, TextField, Typography } from '@mui/material';
 import { Product } from '../../../@types/types';
 import Image from 'next/image';
-import { convertPrice } from '../../../utils';
+import { useShoppingCar } from '../../../hooks/shoppingCar';
 
 interface ProductCardProps {
   product: Product;
@@ -10,17 +10,23 @@ interface ProductCardProps {
 export const ProductCard = ({ product }: ProductCardProps) => {
   const [quantityProduct, setQuantityProduct] = useState(1);
 
+  const { removeCar } = useShoppingCar();
+
   const convertedPrice =
     (Number(product.price_in_cents) / 100) * quantityProduct;
 
   return (
-    <Box className="w-[736px] rounded-md	overflow-hidden bg-white flex gap-x-8 ">
+    <Box className="w-[736px] h-[211px] rounded-md	overflow-hidden bg-white flex gap-x-8 ">
       <Image src={product.image_url} width={256} height={211} />
 
       <Box className="w-full  text-text-gray   pl-0 p-4 flex flex-col gap-4">
         <Box className="flex justify-between items-center">
           <Typography variant="h6">{product.name}</Typography>
-          <IconButton>
+          <IconButton
+            onClick={() => {
+              removeCar(product.id);
+            }}
+          >
             <img src="/images/trash.svg" />
           </IconButton>
         </Box>
@@ -33,9 +39,8 @@ export const ProductCard = ({ product }: ProductCardProps) => {
           <TextField
             type="number"
             defaultValue={quantityProduct}
-            className="w-[64px]"
+            className="w-[64px] h-[40px] color-[#A8A8B3]"
             onChange={(e) => {
-              console.log(e.currentTarget.value);
               setQuantityProduct(Number(e.currentTarget.value));
             }}
             InputProps={{
